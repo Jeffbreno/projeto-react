5dafeae7d87cd4f13bb9b9a10c339fdb69649efc
Jefferson Lopes <jeffbreno@gmail.com>
2023-04-12 18:23:18 -0300

    12042023_1823

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-// import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar, SafeAreaView } from 'react-native';
 
-import Cesta from './src/view/Cesta'; 
+import Cesta from './src/view/Cesta';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView>
+      <StatusBar />
       <Cesta />
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
