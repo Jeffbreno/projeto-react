d66c024f15635e97ef198c2b1942e52a4bb17da3
Jefferson Lopes <jeffbreno@gmail.com>
2023-04-14 00:32:27 -0300

    14042023_0032

--- src/view/Cesta.js
+++ src/view/Cesta.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
 
+import Texto from '../components/Texto';
+
 import topo from '../../assets/topo.png';
 import logo from '../../assets/logo.png'
 
@@ -9,21 +11,21 @@ const width = Dimensions.get('screen').width;
 export default function Cesta() {
     return <>
         <Image source={topo} style={estilos.topo} />
-        <Text style={estilos.titulo}>Detalhes da Cesta</Text>
+        <Texto style={estilos.titulo}>Detalhes da Cesta</Texto>
 
         <View style={estilos.cesta}>
-            <Text style={estilos.nome}>Cesta de Verduras</Text>
+            <Texto style={estilos.nome}>Cesta de Verduras</Texto>
             <View style={estilos.fazenda}>
                 <Image source={logo} style={estilos.imagemFazenda} />
-                <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+                <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
 
             </View>
-            <Text style={estilos.descricao}>
+            <Texto style={estilos.descricao}>
                 Uma cesta com produtos selecionados
                 cuidadosamente da fazenda direto para
                 sua cozinha
-            </Text>
-            <Text style={estilos.preco}>R$ 40,00</Text>
+            </Texto>
+            <Texto style={estilos.preco}>R$ 40,00</Texto>
         </View>
     </>
 }
@@ -53,7 +55,7 @@ const estilos = StyleSheet.create({
         color: "#464646",
         fontSize: 26,
         lineHeight: 42,
-        fontFamily: "MontserratBold",
+        fontWeight: "bold"
     },
     fazenda: {
         flexDirection: "row",
@@ -67,7 +69,6 @@ const estilos = StyleSheet.create({
         fontSize: 16,
         lineHeight: 26,
         marginLeft: 12,
-        fontFamily: "MontserratRegular"
     },
     descricao: {
         color: "#A3A3A3",
